275455d39e61ca5cd90774c7a186230ab17e5b40
Carlos E <github@capitan.simplelogin.com>
2024-12-12 12:12:29 -0600

    Initialized project and began methods for dictionary class

---
+++ lib/dictionary.rb
@@ -0,0 +1,27 @@
+class Dictionary
+  attr_accessor :chosen_word
+
+  def initialize
+    @chosen_word = ""
+  end
+
+  def load_in
+    dictionary = []
+    contents = File.readlines('../google-10000-english-no-sweats.txt')
+    contents.each do |word|
+
+      dictionary << word
+    end
+    dictionary
+  end
+
+  def choose_random_word
+    dictionary = self.load_in()
+    criteria_words = dictionary.select {|word| (word.length >= 5 && word.length <= 12)}
+    chosen_word = criteria_words.sample()
+    chosen_word
+  end
+end
+
+test = Dictionary.new
+puts test.choose_random_word()
